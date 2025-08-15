bee77337055e220a4ad3d74a215a65f0fce6d87d
PacoCanovaca <pacocanovaca97@gmail.com>
2025-08-15 14:21:31 +0200

    Ejercicio 5 DOM desarrollado y terminado

--- ejercicios-dom/ejercicio-5/app.js
+++ ejercicios-dom/ejercicio-5/app.js
@@ -1,19 +1,27 @@
+// Voy a modificar el array añadiendo la propiedad de src para incluir imágenes de los álbums (de cara a hacer la web más bonita)
+
 const albums = [
-  "De Mysteriis Dom Sathanas",
-  "Reign of Blood",
-  "Ride the Lightning",
-  "Painkiller",
-  "Iron Fist",
+  { name: "De Mysteriis Dom Sathanas", src: "./assets/icons/Mysteriis.jpg" },
+  { name: "Reign of Blood", src: "./assets/icons/Reign.jpg" },
+  { name: "Ride the Lightning", src: "./assets/icons/Ride.jpg" },
+  { name: "Painkiller", src: "./assets/icons/Pain.jpg" },
+  { name: "Iron Fist", src: "./assets/icons/Iron.jpg" },
 ];
 
 function generateAlbums(albumList) {
-    const ul = document.createElement("ul");
-    for (const album of albumList) {
-        const li = document.createElement("li");
-        li.innerText = album;
-        ul.appendChild(li);
-    }
-    document.body.appendChild(ul);
-};
+  const ul = document.createElement("ul");
+  for (const album of albumList) {
+    const li = document.createElement("li");
+    const h3 = document.createElement("h3");
+    const img = document.createElement("img");
+    h3.innerText = album.name;
+    img.src = album.src;
+    li.appendChild(h3);
+    li.appendChild(img);
+    ul.appendChild(li);
+  }
+  const main = document.querySelector("main");
+  main.appendChild(ul);
+}
 
-generateAlbums(albums)
+generateAlbums(albums);
